8f40f8b71c190529ad328adb4446f2ca1968ac89
Yasir Dönmez <yasirdonmez12345@gmail.com>
2026-01-21 11:22:16 +0300

    Debug: Add simple app/page.tsx to verify root routing bypassing groups

---
+++ novelytical-client/app/page.tsx
@@ -0,0 +1,10 @@
+
+export const runtime = 'nodejs';
+
+export default function RootPage() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">Root Page Works!</h1>
+        </div>
+    );
+}
